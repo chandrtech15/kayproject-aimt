5b4278692294257a9f38de60eb1c8ebc8103087f
kellerpforte <kellerpforte@1d377dd0-822c-d795-7724-41c493a2a4c6>
2011-08-15 22:37:04 +0000

    Created makefiles

--- trunk/corpus/splitCorpusIntoFiles.sh
+++ trunk/corpus/splitCorpusIntoFiles.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-awk -v fname='' '/^.I / {close(fname);id=$2;fname=FILENAME"."id;next} \
+awk -v fname='' -v n=0 -v base=$3 '/^.I / {id=$2; if(n >= '$2' || n == 0){close(fname);fname=base"."id;n=0;}n++;next} \
             /^.W$/ {W=1;next} \
             W {print("ID "id) > fname;print > fname;W=0;next}' $1
