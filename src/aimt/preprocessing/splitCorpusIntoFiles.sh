51585b9155d7abfad1e51ccf81f3bcd23a8e5cfa
kellerpforte <kellerpforte@1d377dd0-822c-d795-7724-41c493a2a4c6>
2011-09-24 13:39:16 +0000

    Rewrote corpus chunking script

--- src/aimt/preprocessing/splitCorpusIntoFiles.sh
+++ src/aimt/preprocessing/splitCorpusIntoFiles.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
-awk -v fname='' -v n=0 -v base=$3 '/^.U / {id=$2; if(n >= '$2' || n == 0){close(fname);fname=base"."id;n=0;}n++;next} \
-            /^.W$/ {W=1;next} \
-            W {print(".I "id) > fname;print > fname;W=0;next}' $1
+if [ $# -ne 3 ]
+then
+	echo "Usage: corpus-file-to-split chunk-size base-file-name-of-splitted-files";
+	exit 1;
+fi
+print $2;
+awk -v base=$3 -v fname=$3".1" -v chunkSize=$2 \
+	'	BEGIN {n=0;fname = base"."n;print "Writing to "fname}
+		/^.U$/ {
+			if(text) {print text > fname;}
+			if((n % chunkSize) == 0) {fname = base"."n; print "Writing to "fname}
+			text = "";readId = 1;n += 1;next
+		}
+		/^[0-9]/ && readId == 1 {print(".I "$0) > fname;readId=0;next}
+		/^.T$/ {T=1;next} 
+	     T {text=$0;T=0;next}
+	     /^.W$/ {W=1;next}
+	     W {text=text" "$0;W=0;next}
+		END {print text > fname;}' $1
